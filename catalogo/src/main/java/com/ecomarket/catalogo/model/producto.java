81c376c6607e32ed3a5adda83183652e2fbd2ef2
noahgonzalez <noa.gonzalez@duocuc.cl>
2026-03-27 15:10:22 -0300

    Backend EcoMarket

---
+++ catalogo/src/main/java/com/ecomarket/catalogo/model/producto.java
@@ -0,0 +1,30 @@
+package com.ecomarket.catalogo.model;
+
+public class producto {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private int precio;
+    private String imagenUrl;
+
+    public producto() {}
+
+    public producto(Long id, String nombre, String descripcion, int precio, String imagenUrl) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.imagenUrl = imagenUrl;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public int getPrecio() { return precio; }
+    public void setPrecio(int precio) { this.precio = precio; }
+    public String getImagenUrl() { return imagenUrl; }
+    public void setImagenUrl(String imagenUrl) { this.imagenUrl = imagenUrl; }
+}
